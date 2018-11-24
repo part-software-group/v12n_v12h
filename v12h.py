8ddccf2e1a2244d12795fdd5ecbee70ac90fdf3e
fakhraee <fakhraee@riseup.net>
2018-11-24 12:05:32 +0330

    fixes

--- v12h.py
+++ v12h.py
@@ -12,23 +12,26 @@ show, password_num = False, 12
 parser = argparse.ArgumentParser()
 parser.add_argument( "--list", "-l", help="list vms", action="store_true" )
 parser.add_argument( "--status", "-s", help="list with status on|off", nargs=1 )
-parser.add_argument( "--create-user", "-a", help="create user", nargs=1 )
-parser.add_argument( "--create-vm", "-m", help="create vm", nargs=1 )
-parser.add_argument( "--password", "-p", help="password", nargs=1 )
-parser.add_argument( "--show", "-w", help="show password", action="store_true" )
+parser.add_argument( "--create-user", help="create user", nargs=1 )
+parser.add_argument( "--create-vm", help="create vm", nargs=1 )
+parser.add_argument( "--password", help="password", nargs=1 )
+parser.add_argument( "--show", help="show password", action="store_true" )
+parser.add_argument( "--virsh", "-i", help="virsh of vm" )
+parser.add_argument( "--edit", "-e", help="edit vm xml" )
 parser.add_argument( "--console", "-n", help="console to vm" )
-parser.add_argument( "--start", "-r", help="start vm" )
-parser.add_argument( "--stop", "-D", help="stop vm" )
-parser.add_argument( "--add-bridge", "-b",
-                     help="adding bridge stuff",
-                     action="store_true" )
-parser.add_argument( "--bridge-ip", "-I", help="ip address to set on bridge" )
+parser.add_argument( "--start", "-R", help="start vm" )
+parser.add_argument( "--stop", "-S", help="stop vm" )
+parser.add_argument( "--set-cpu", help="set vm vpu to (if maxcpu)" )
+parser.add_argument( "--set-mem", help="set vm memory to (if maxmem)" )
+parser.add_argument( "--add-bridge", help="add bridge on, ex: eth0" )
+parser.add_argument( "--bridge-if", help="name of the bridge, ex: br0" )
+parser.add_argument( "--bridge-ip", help="ip address to set on bridge" )
 args = parser.parse_args()
 
 def check_root():
     "some functions need root permission"
     if os.geteuid() != 0:
-        exit( "error: not root" )
+        exit( "error: must be root" )
 
 def su_as_vm( vm, command ):
     "su as user that runs vm"
@@ -80,24 +83,31 @@ def listdir_nohidden():
         if not f.startswith( '.' ) and not f == "lost+found":
             yield f
 
-def setup_bridge( br_ip ):
+def setup_bridge( br_on, br_if, br_ip ):
     "setting up bridge interface and qemu related stuff"
     check_root()
-    with open( "/etc/network/inetrfaces.d/br0", "w" ) as br:
-        br.write( "auto br0\n"
-                  "iface br0 inet static\n"
-                  "  address " + br_ip + "\n"
-                  "  bridge_ports eth0\n"
+    bo = str( br_on )
+    bf = str( br_if )
+    bi = str( br_ip )
+    with open( "/etc/network/interfaces.d/" + bf, "w+" ) as br:
+        br.write( "auto " + bf + "\n"
+                  "iface " + bf + " inet static\n"
+                  "  address " + bi + "\n"
+                  "  bridge_ports " + bo + "\n"
                   "  bridge_stp off\n"
                   "  bridge_fd 0\n" )
-    with open( "/etc/qemu/bridge.conf", "w" ) as bc:
-        bc.write( "allow br0\n" )
-    subprocess.run( [ "ip", "address", "flush", "eth0", "scope", "global" ] )
-    subprocess.run( [ "ifup", "br0" ] )
+    with open( "/etc/qemu/bridge.conf", "w+" ) as bc:
+        bc.write( "allow " + bf + "\n" )
+    subprocess.run( [ "ip", "address", "flush", bo, "scope", "global" ] )
+    subprocess.run( [ "ifup", bf ] )
     subprocess.run( [ "setcap",
                       "cap_net_admin+ep",
                       "/usr/lib/qemu/qemu-bridge-helper" ] )
 
+if args.virsh:
+    su_as_vm( args.virsh, "virsh" )
+if args.edit:
+    su_as_vm( args.edit, "virsh edit " + args.edit )
 if args.console:
     su_as_vm( args.console, "virsh console " + args.console ) 
 if args.start:
@@ -138,7 +148,8 @@ if args.create_vm:
     create_vm( vm = args.create_vm[0] )
 
 if args.add_bridge:
-    if args.bridge_ip:
-        setup_bridge( br_ip = args.bridge_ip )
-    else:
-        exit( "error: no ip address defined" )
+    if args.bridge_if:
+        if args.bridge_ip:
+            setup_bridge( br_on = args.add_bridge,
+                          br_if = args.bridge_if,
+                          br_ip = args.bridge_ip )
